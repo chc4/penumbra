445ef599fa6ba6e335cb2f3c982b585371496911
redshiftzero <jen@penumbra.zone>
2021-10-08 16:33:57 -0400

    key agreement proptest

--- decaf377-ka/src/lib.rs
+++ decaf377-ka/src/lib.rs
@@ -1,17 +1,14 @@
+use std::convert::TryFrom;
+
 use ark_ff::UniformRand;
+use ark_serialize::CanonicalDeserialize;
 
 use decaf377;
 use rand_core::{CryptoRng, RngCore};
 
 /// A `SharedSecret` derived at the end of the key agreement protocol.
 #[derive(Debug, PartialEq)]
-pub struct SharedSecret(pub(crate) decaf377::Element);
-
-impl SharedSecret {
-    pub fn derive(other_public_key: &EphemeralPublicKey, secret_key: EphemeralSecretKey) -> Self {
-        Self(other_public_key.0 * secret_key.0)
-    }
-}
+pub struct SharedSecret(pub [u8; 32]);
 
 /// An `EphemeralSecretKey` is used once and consumed when forming a `SharedSecret`.
 pub struct EphemeralSecretKey(pub(crate) decaf377::Fr);
@@ -24,6 +21,19 @@ impl EphemeralSecretKey {
     pub fn derive_public(&self) -> EphemeralPublicKey {
         EphemeralPublicKey(self.0 * decaf377::Element::basepoint())
     }
+
+    pub fn key_agreement_with(self, other: &EphemeralPublicKey) -> SharedSecret {
+        SharedSecret((other.0 * self.0).compress().into())
+    }
+}
+
+impl TryFrom<[u8; 32]> for EphemeralSecretKey {
+    type Error = ark_serialize::SerializationError;
+
+    fn try_from(bytes: [u8; 32]) -> Result<Self, Self::Error> {
+        let secret = decaf377::Fr::deserialize(&bytes[..])?;
+        Ok(EphemeralSecretKey(secret))
+    }
 }
 
 /// An `EphemeralPublicKey` sent to the other participant in the key agreement protocol.
@@ -32,6 +42,7 @@ pub struct EphemeralPublicKey(pub(crate) decaf377::Element);
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
 
     #[test]
     fn test_key_agreement_happy_path() {
@@ -44,9 +55,28 @@ mod tests {
         let alice_pubkey = alice_secret.derive_public();
         let bob_pubkey = bob_secret.derive_public();
 
-        let alice_sharedsecret = SharedSecret::derive(&bob_pubkey, alice_secret);
-        let bob_sharedsecret = SharedSecret::derive(&alice_pubkey, bob_secret);
+        let alice_sharedsecret = alice_secret.key_agreement_with(&bob_pubkey);
+        let bob_sharedsecret = bob_secret.key_agreement_with(&alice_pubkey);
 
         assert_eq!(alice_sharedsecret, bob_sharedsecret);
+    }
+
+    proptest! {
+        #[test]
+        fn key_agreement(
+            alice_bytes in prop::array::uniform32(any::<u8>()),
+            bob_bytes in prop::array::uniform32(any::<u8>()),
+        ) {
+            let alice_secret = EphemeralSecretKey::try_from(alice_bytes).expect("test alice secret is valid");
+            let bob_secret = EphemeralSecretKey::try_from(bob_bytes).expect("test bob secret is valid");;
+
+            let alice_pubkey = alice_secret.derive_public();
+            let bob_pubkey = bob_secret.derive_public();
+
+            let alice_sharedsecret = alice_secret.key_agreement_with(&bob_pubkey);
+            let bob_sharedsecret = bob_secret.key_agreement_with(&alice_pubkey);
+
+            assert_eq!(alice_sharedsecret, bob_sharedsecret);
+        }
     }
 }
